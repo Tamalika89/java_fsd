7701d1b35614bd296c7c751786992a74b562b106
Tamalika <tamalika.dey007@gmail.com>
2023-05-26 17:22:40 +0530

    commit

---
+++ database/src/dbUtil/dbMain.java
@@ -0,0 +1,20 @@
+package dbUtil;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class dbMain {
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	Connection con=Dbutil.getConn();
+	if(con==null) {
+		System.out.println("connection failed");
+	}
+	else {
+		System.out.println("connection is successfully established ....!!!");
+	}
+}
+}
+
+
