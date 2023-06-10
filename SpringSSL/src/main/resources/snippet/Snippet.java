374c7f3b74bbdef196027c95d3569ee30c8d7e7c
Tamalika <tamalika.dey007@gmail.com>
2023-06-10 17:47:38 +0530

    Commit

---
+++ SpringSSL/src/main/resources/snippet/Snippet.java
@@ -0,0 +1,7 @@
+package snippet;
+
+public class Snippet {
+	The JKS keystore uses a proprietary format. It is recommended to migrate to PKCS12 which is an industry standard format using "keytool -importkeystore -srckeystore ssl-server.jks -destkeystore ssl-server.jks -deststoretype pkcs12".
+	
+}
+
